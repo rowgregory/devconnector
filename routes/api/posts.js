e67be0398fc887b9f965565460563a6b9de02cb5
rowgregory <rowgregory@gmail.com>
2019-08-01 21:51:51 -0400

    React set up & initial components

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -17,13 +17,13 @@ const {
 } = PostController;
 
 /** @access    Private */
-router.post("/", [auth, [createdPost]], createPost);
 router.get("/", auth, getAllPosts);
 router.get("/:id", auth, getPostById);
-router.delete("/:id", auth, deletePost);
+router.post("/", [auth, [createdPost]], createPost);
+router.post("/comment/:id", [auth, [createdPost]], commentOnPost);
 router.put("/like/:id", auth, likePost);
 router.put("/unlike/:id", auth, unlikePost);
-router.post("/comment/:id", [auth, [createdPost]], commentOnPost);
+router.delete("/:id", auth, deletePost);
 router.delete("/comment/:id/:comment_id", auth, deleteComment);
 
 module.exports = router;
